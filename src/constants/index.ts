9481a1882512e15cae1853169e1021924e204bb9
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-08-17 18:26:37 +0300

    UM-9: move benefitsOfRegistration to a constants folder

---
+++ src/constants/index.ts
@@ -0,0 +1,17 @@
+export const benefitsOfRegistration = [
+  {
+    title: 'Honey never spoils',
+    description:
+      'Archaeologists have found pots of honey in ancient Egyptian tombs that are over 3,000 years old and still perfectly edible.',
+  },
+  {
+    title: 'Octopuses have three hearts',
+    description:
+      'These fascinating creatures possess three hearts—one main heart that pumps blood through the entire body and two additional hearts that pump blood through the gills.',
+  },
+  {
+    title: 'The shortest war in history',
+    description:
+      'The Anglo-Zanzibar War, which occurred on August 27, 1896, holds the record for being the shortest war ever recorded.',
+  },
+];
