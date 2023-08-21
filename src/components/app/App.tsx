5f66ade7443dde57b8a6bdda6e6a0a5980b887ff
Nick Yefimov <kometmontana22@gmail.com>
2023-08-22 00:50:17 +0300

    added console logs,and checkAuth on init app

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,8 +1,20 @@
-import { SignUp } from '../../pages/SignUp';
+import { useEffect } from 'react';
 
+import { SignUp } from '../../pages/SignUp';
+import { checkAuth } from '../../store/Auth/authActions';
+import { useAppSelector } from '../../hooks/reduxHook';
 const App: React.FC = () => {
+  const { isAuth, user } = useAppSelector((state) => state.authSlice);
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      checkAuth();
+    }
+  }, []);
+
   return (
     <div className='h-screen w-full'>
+      <h1>{isAuth ? `user auth ${user.email}` : 'not auth'}</h1>
       <SignUp />
     </div>
   );
