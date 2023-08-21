a6523856022fe6a16f5b735ff2bdefc8ac64014b
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-08-21 14:10:05 +0300

    UM-18: add routes to App

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,16 @@
 import { SignUp } from '../../pages/SignUp';
+import { Routes, Route } from 'react-router';
+import { SignIn } from '../../pages/SignIn';
+import { RegistrationBenefits } from '../auth/RegistrationBenefits';
 
 const App: React.FC = () => {
   return (
-    <div className='h-screen w-full'>
-      <SignUp />
+    <div className='h-screen w-full grid grid-cols-[50%_50%]'>
+      <RegistrationBenefits />
+      <Routes>
+        <Route path='/' element={<SignUp />} />
+        <Route path='/signin' element={<SignIn />} />
+      </Routes>
     </div>
   );
 };
