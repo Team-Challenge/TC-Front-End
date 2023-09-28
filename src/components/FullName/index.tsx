1a8510b931919a89d3e452a8a463ec730db938a5
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-09-28 15:22:32 +0300

    fix: delete error message

--- src/components/FullName/index.tsx
+++ src/components/FullName/index.tsx
@@ -1,25 +1,16 @@
-import { useFormContext } from 'react-hook-form';
 import { TextInput } from '../UI/TextInput';
-import s from './FullName.module.scss';
 
 export const FullName = () => {
-  const {
-    formState: { errors },
-  } = useFormContext();
-
   return (
-    <>
-      <TextInput
-        type='text'
-        id='full_name'
-        placeholder='Full Name'
-        required={true}
-        regex={/^[a-zA-Z\xC0-\uFFFF]+([ \-']{0,1}[a-zA-Z\xC0-\uFFFF]+){3,5}[.]{0,1}$/}
-        errorMessage='Only letters'
-        maxLength={50}
-        maxLengthMessage='The name is too long'
-      />
-      {errors.full_name && <p className={`${s.error}`}>{errors.full_name.message as string}</p>}
-    </>
+    <TextInput
+      type='text'
+      id='full_name'
+      placeholder='Full Name'
+      required={true}
+      regex={/^[a-zA-Z\xC0-\uFFFF]+([ \-']{0,1}[a-zA-Z\xC0-\uFFFF]+){3,5}[.]{0,1}$/}
+      errorMessage='Only letters'
+      maxLength={50}
+      maxLengthMessage='The name is too long'
+    />
   );
 };
