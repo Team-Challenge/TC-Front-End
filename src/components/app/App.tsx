5834e3acc33e482728a5490eb1aeec0dca05ba6a
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-10-03 17:30:36 +0300

    feat: add 404 page

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect } from 'react';
-import { Routes, Route } from 'react-router';
+import { Routes, Route } from 'react-router-dom';
 import { SignIn, SignUp, UserPanel } from '../../pages';
 import { checkAuth } from '../../store/auth/authActions';
+import { PageNotFound } from '../../pages/PageNotFound';
 
 const App: React.FC = () => {
   useEffect(() => {
@@ -16,6 +17,7 @@ const App: React.FC = () => {
         <Route path='/' element={<SignUp />} />
         <Route path='/signin' element={<SignIn />} />
         <Route path='/userpanel' element={<UserPanel />} />
+        <Route path="*" element={<PageNotFound />} />
       </Routes>
     </div>
   );
