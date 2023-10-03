6f67099c5d6e24b099d053c06406ecb2b427f6df
Iryna Petrenko <iryna.petrenko4@gmail.com>
2023-10-03 13:24:58 +0300

    feat: add disabled for btn

--- src/components/UI/ButtonUI/index.tsx
+++ src/components/UI/ButtonUI/index.tsx
@@ -5,14 +5,21 @@ interface ButtonUIProps {
   variant?: string;
   className?: string;
   onClick?: () => void;
+  disabled?: boolean;
 }
 
-export const ButtonUI = ({ label, variant = 'main', className, onClick }: ButtonUIProps) => {
+export const ButtonUI = ({
+  label,
+  variant = 'main',
+  className,
+  onClick,
+  disabled = false,
+}: ButtonUIProps) => {
   const btnStyle = `${s.btn} ${className}
   ${variant === 'main' ? s.main : s.secondary}`;
 
   return (
-    <button className={btnStyle} onClick={onClick}>
+    <button className={btnStyle} onClick={onClick} disabled={disabled}>
       {label}
     </button>
   );
