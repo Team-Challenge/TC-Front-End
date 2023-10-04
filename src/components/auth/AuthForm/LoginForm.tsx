010c66bc6c8f6974750d6feb9174c81dd2dee6dc
Nick Yefimov <kometmontana22@gmail.com>
2023-10-04 19:19:14 +0300

    added function with redirect after autorisation

--- src/components/auth/AuthForm/LoginForm.tsx
+++ src/components/auth/AuthForm/LoginForm.tsx
@@ -4,19 +4,28 @@ import { AuthButton } from '../AuthButton';
 import { PasswordInput } from '../../UI/PasswordInput';
 import s from './AuthForm.module.scss';
 import { Email } from '../../Email';
-import { useAppDispatch } from '../../../hooks/reduxHook';
+import { useAppDispatch, useAppSelector } from '../../../hooks/reduxHook';
 import { login } from '../../../store/auth/authThunks';
+import { useNavigate } from 'react-router';
 
 export const LoginForm = () => {
   const methods = useForm({
     mode: 'onChange',
   });
 
+  const navigate = useNavigate();
+  const { isAuth } = useAppSelector((state) => state.auth);
   const dispatch = useAppDispatch();
 
   const {
     formState: { errors, isValid },
   } = methods;
+
+  const isUserAuth = () => {
+    if (isAuth) {
+      navigate('/userpanel');
+    }
+  };
 
   const onSubmit: SubmitHandler<IUserAuth> = (data) => {
     dispatch(login(data));
@@ -34,12 +43,7 @@ export const LoginForm = () => {
         <PasswordInput id='password' placeholder='Pass' />
         {errors.password && <p className={`${s.error}`}>{errors.password.message as string}</p>}
 
-        <AuthButton
-          text='Далі'
-          variant='main'
-          disabled={!isValid}
-          // onClick={}
-        />
+        <AuthButton text='Далі' variant='main' disabled={!isValid} onClick={isUserAuth} />
       </form>
     </FormProvider>
   );
