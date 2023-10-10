cd3c98953f93f19970499213e5bef841e83fd515
Nick Yefimov <kometmontana22@gmail.com>
2023-10-10 15:49:44 +0300

    added setPhoneNumber action in store

--- src/store/userSettings/userSettingsSlice.ts
+++ src/store/userSettings/userSettingsSlice.ts
@@ -2,10 +2,12 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface UserState {
   full_name: string;
+  phone_number: string;
 }
 
 const initialState = {
   full_name: '',
+  phone_number: '',
 } as UserState;
 
 const userSettingsSlice = createSlice({
@@ -15,9 +17,12 @@ const userSettingsSlice = createSlice({
     setFullName: (state, action: PayloadAction<string>) => {
       state.full_name = action.payload;
     },
+    setPhoneNumber: (state, action: PayloadAction<string>) => {
+      state.phone_number = action.payload;
+    },
   },
 });
 
-export const { setFullName } = userSettingsSlice.actions;
+export const { setFullName, setPhoneNumber } = userSettingsSlice.actions;
 
 export default userSettingsSlice.reducer;
