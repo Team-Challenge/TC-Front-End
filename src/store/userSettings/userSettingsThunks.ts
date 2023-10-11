d52967dc7aace82809b988076991e6bb60335a25
Nick Yefimov <kometmontana22@gmail.com>
2023-10-11 21:03:36 +0300

    added request to change password

--- src/store/userSettings/userSettingsThunks.ts
+++ src/store/userSettings/userSettingsThunks.ts
@@ -1,5 +1,5 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import { setFullName, setPhoneNumber } from './userSettingsSlice';
+import { setFullName, setPassword, setPhoneNumber } from './userSettingsSlice';
 import axios from 'axios';
 
 export const changeFullName = createAsyncThunk(
@@ -47,6 +47,32 @@ export const changePhoneNumber = createAsyncThunk(
       );
       if (response.status === 200) {
         dispatch(setPhoneNumber);
+        return response.data;
+      }
+    } catch (e) {
+      const error = e as Error;
+      throw error;
+    }
+  },
+);
+
+export const changePassword = createAsyncThunk(
+  'userSettings/changePassword',
+  async (newPassword: string, { dispatch }) => {
+    try {
+      const token = localStorage.getItem('token');
+      const headers = {
+        Authorization: `Bearer ${token}`,
+      };
+      const response = await axios.post(
+        'http://207.154.197.128:8080/accounts/change_password',
+        {
+          new_password: newPassword,
+        },
+        { headers },
+      );
+      if (response.status === 200) {
+        dispatch(setPassword);
         return response.data;
       }
     } catch (e) {
