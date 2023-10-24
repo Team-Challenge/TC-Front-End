9f745f7ffb7268d54f171aa982d0ec8f8e341f4c
Nick Yefimov <kometmontana22@gmail.com>
2023-10-24 17:08:46 +0300

    change name of env variable for right work;fix base url;

--- src/http/index.ts
+++ src/http/index.ts
@@ -1,8 +1,7 @@
 import axios from 'axios';
 import { AuthResponse } from '../types';
 
-export const BASE_URL = process.env.API_URL ? process.env.API_URL : 'http://207.154.197.128:8080';
-
+export const BASE_URL = process.env.REACT_APP_API_URL;
 
 const $api = axios.create({
   withCredentials: true,
